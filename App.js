f1fd4902cc4ecfb9368d65c404daefd8c6b83093
dino buljubasic <dbnex14@gmail.com>
2018-02-04 20:04:03 -0800

    Added SearchPage and house images.

--- App.js
+++ App.js
@@ -5,32 +5,11 @@
  */
 'use strict'; // enable strict mode to add improved editor handling
 
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text
-} from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
-class SearchPage extends Component<{}> {
-  static navigationOptions = { title: 'Property Finder'};
-
-  render() {
-    return <Text style={styles.description}>Search for houses to buy!</Text>;
-  }
-}
+import SearchPage from './SearchPage';
 
 const App = StackNavigator({
   Home: { screen: SearchPage },
 });
 
 export default App;
-
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  }
-});
